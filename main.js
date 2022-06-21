2a2a454672de6932d70601d2080dc13ce187f702
Andressa Helena <handressa125@gmail.com>
2022-06-21 15:37:17 -0300

    Modal Test

---
+++ main.js
@@ -0,0 +1,29 @@
+const modal = document.querySelector('#abrirModal');
+const modalBtn = document.querySelector('#modal-btn');
+const closeBtn = document.querySelector('.close');
+
+
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
+
+
+function openModal() {
+  modal.style.display = 'block';
+  modalBtn.style.display = 'none';
+}
+
+
+function closeModal() {
+  modal.style.display = 'none';
+  modalBtn.style.display = 'block';
+}
+
+
+function outsideClick(e) {
+  if (e.target == modal) {
+    modal.style.display = 'none';
+    modalBtn.style.display = 'block';
+  }
+}
+
